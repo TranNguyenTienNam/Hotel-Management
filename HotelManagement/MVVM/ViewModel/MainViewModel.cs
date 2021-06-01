e0b96da25fa162e713f42215e8c020f424338588
Tiến Nam <19521871@gm.uit.edu.vn>
2021-06-01 21:17:38 +0700

    Add project files.

---
+++ HotelManagement/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using HotelManagement.Core;
+
+namespace HotelManagement.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand BookingsViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+
+        public BookingsViewModel BookingsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            BookingsVM = new BookingsViewModel();
+            
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVM;
+            });
+
+            BookingsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = BookingsVM;
+            });
+        }
+    }
+}
---
+++ HotelManagement/RegisterWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelManagement
+{
+    /// <summary>
+    /// Interaction logic for RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+        private void btnFinish_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            (new MainWindow()).Show();
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            (new LoginWindow()).Show();
+            this.Close();
+        }
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
