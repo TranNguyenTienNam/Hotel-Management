783530d7c7246e2e8d299422711e1e5b943dd724
pxuanbach <pxuanbach1412@gmail.com>
2021-06-06 13:01:43 +0700

    first commit

---
+++ HotelManagement/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using HotelManagement.Core;
+
+namespace HotelManagement.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand BookingsViewCommand { get; set; }
+
+        public RelayCommand RoomsViewCommand { get; set; }
+
+        public RoomsViewModel RoomsVM { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+
+        public BookingsViewModel BookingsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            RoomsVM = new RoomsViewModel();
+            BookingsVM = new BookingsViewModel();
+            
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVM;
+            });
+
+            RoomsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = RoomsVM;
+            });
+
+            BookingsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = BookingsVM;
+            });
+        }
+    }
+}
---
+++ HotelManagement/Object/user.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement
+{
+    class user
+    {
+        string userName;
+        string passWord;
+        string ho;
+        string ten;
+        string soDienThoai;
+        string gioiTinh;
+        string email;
+        DateTime ngaySinh;
+        string quyenHan;
+        string tinhTrangTK;
+
+        public string UserName { get => userName; set => userName = value; }
+        public string PassWord { get => passWord; set => passWord = value; }
+        public string Ho { get => ho; set => ho = value; }
+        public string Ten { get => ten; set => ten = value; }
+        public string Email { get => email; set => email = value; }
+        public string QuyenHan { get => quyenHan; set => quyenHan = value; }
+        public string TinhTrangTK { get => tinhTrangTK; set => tinhTrangTK = value; }
+        public string SoDienThoai { get => soDienThoai; set => soDienThoai = value; }
+        public DateTime NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+        public string GioiTinh { get => gioiTinh; set => gioiTinh = value; }
+    }
+}
