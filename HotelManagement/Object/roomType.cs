639829cf8f3f6e3adef4bd9a131f633e407f9408
voquocminh <19521857@gm.uit.edu.vn>
2021-06-21 16:20:29 +0700

    add object roomtype

---
+++ HotelManagement/Object/roomType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Object
+{
+    class roomType
+    {
+        int maLoaiPhong;
+        string tenLoaiPhong;
+        decimal donGia;
+        int soNgToiDa;
+
+        public int MaLoaiPhong { get => maLoaiPhong; set => maLoaiPhong = value; }
+        public string TenLoaiPhong { get => tenLoaiPhong; set => tenLoaiPhong = value; }
+        public decimal DonGia { get => donGia; set => donGia = value; }
+        public int SoNgToiDa { get => soNgToiDa; set => soNgToiDa = value; }
+    }
+}
