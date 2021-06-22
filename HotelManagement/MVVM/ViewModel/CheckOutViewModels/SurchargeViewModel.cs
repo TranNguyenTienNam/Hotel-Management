f888327d8c03256673bcb3a827bfc235451d0eda
voquocminh <19521857@gm.uit.edu.vn>
2021-06-22 22:52:10 +0700

    update

--- HotelManagement/MVVM/ViewModel/CheckOutViewModels/SurchargeViewModel.cs
+++ HotelManagement/MVVM/ViewModel/CheckOutViewModels/SurchargeViewModel.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HotelManagement.MVVM.ViewModel
+namespace HotelManagement.MVVM.ViewModel.CheckOutViewModels
 {
-    class BookingsViewModel
+    class SurchargeViewModel
     {
-
     }
 }
