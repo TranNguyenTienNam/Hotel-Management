f09230ef50f949329f7ab71ed228e003cea01809
pxuanbach <pxuanbach1412@gmail.com>
2021-06-27 16:23:39 +0700

    create loginViewModel

---
+++ HotelManagement/MVVM/ViewModel/Login/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using HotelManagement.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelManagement.MVVM.Model;
+using System.Windows.Input;
+using System.Data;
+
+namespace HotelManagement.MVVM.ViewModel
+{
+    class LoginViewModel : ObservableObject
+    {
+
+    }
+}
